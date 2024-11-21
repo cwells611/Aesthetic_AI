c15f5284d32ce0165462a7fa304e61b8453942a2
Caden <cwells@mines.edu>
2024-11-21 01:52:11 -0700

    added function to generate image based off user input

--- app.py
+++ app.py
@@ -8,15 +8,6 @@ import requests
 import base64
 import pandas as pd
 
-#function that will search throgh furniture csv and return filtered dataframe based on list of keywords 
-def piece_df(csv_file, keywords):
-   #read in csv file
-   df = pd.read_csv(csv_file)
-   #create a new dataframe that only contains the rows where any of the keywords is in the image_title column
-   new_df = df[df['image_title'].str.contains('|'.join(keywords), case=False, na=False)]
-   return new_df
-
-
 #get the api key from the .env file and create am openai clinet 
 _ = load_dotenv(find_dotenv())
 client = OpenAI(api_key=os.getenv('OPEN_API_KEY'))
@@ -34,26 +25,38 @@ image_path = os.path.join(image_dir, image_name)
 if not os.path.isdir(image_dir):
    os.mkdir(image_dir)
 
-#lists of key words for each piece the user could select for later suggestions and csv file
+#csv file with furniture info 
 furniture_csv = 'furniture.csv'
-coffee_table_keywords = ['coffee table']
-side_table_keywords = ['side table', 'end table', 'accent table']
-couch_keywords = ['couch', 'sofa', 'futon', 'sectional'] 
-entertainment_center_keywords = ['entertainment center', 'media', 'tv stand']
-rug_keywords = ['rug', 'area']
-bed_keywords = ['headboard', 'bed']
-shelves_keywords = ['shelves', 'bookshelves', 'bookcase']
-lamp_keywords = ['lamp']
-dresser_keywords = ['dresser', 'chest', 'wardrobe']
-nightstand_keywords = ['nightstand']
-desk_keywords = ['desk', 'desk chair']
-armchair_keywords = ['armchair', 'recliner', 'loveseat', 'chair']
+
+#dictionary of key words for each piece the user could select for later suggestions and csv file
+keywords = {
+   "coffee_table": ['coffee table'],
+   "side_table": ['side table', 'end table', 'accent table'],
+   "couch": ['couch', 'sofa', 'futon', 'sectional'],
+   "entertainment_center": ['entertainment center', 'media', 'tv stand'],
+   "rug": ['rug', 'area'],
+   "bed": ['headboard', 'bed'],
+   "shelves": ['shelves', 'bookshelves', 'bookcase'],
+   "lamp": ['lamp'],
+   "dresser": ['dresser', 'chest', 'wardrobe'],
+   "nightstand": ['nightstand'],
+   "desk": ['desk', 'desk chair'],
+   "armchair": ['armchair', 'recliner', 'loveseat', 'chair']
+}
 
 #check to see if there is an image in dir, remove if there is
 #only want to display current image, if there is an image in the 
 #folder from a previous run, delete it 
 if os.path.exists(image_path):
    os.remove(image_path)
+
+#function that will search throgh furniture csv and return filtered dataframe based on list of keywords 
+def piece_df(csv_file, keywords, aesthetic):
+   #read in csv file
+   df = pd.read_csv(csv_file)
+   #create a new dataframe that only contains the rows where any of the keywords and aesthetic is in the image_title column
+   new_df = df[df['image_title'].str.contains('|'.join(keywords), case=False, na=False) & df['image_title'].str.contains(aesthetic, case=False, na=False)]
+   return new_df
 
 #function to save generated image for display 
 def save_image(image_url):
@@ -78,7 +81,30 @@ def generate_image(prompt):
    #save image
    save_image(image_url)
 
-#function to generated image based on uploaded image input 
+#function to generate description based on selected aesthetic and furniture pieces 
+def generate_description_user_input(selected_pieces, aesthetic):
+   selected_pieces = list(selected_pieces)
+   #make api call to gpt4o with the selected pieces and the aesthetic 
+   response = client.chat.completions.create(
+      model='gpt-4o',
+      messages=[
+         {
+            "role": "user",
+            "content": [
+               {
+                  "type": "text",
+                  "text": f"You are taking the role of an interior designer. You will be given as overall aesthtic of the room along with a list of furniture items to be included in the room. You job is to design this room within the given aesthetic and correctly incorporating the given furniture items. Feel free to add more than the given items to the room to design it to the best of yoru abilities while reamining realistic and within the constraints. Here is the aesthetic for the room: {aesthetic}, and here is the list of requried furniture that needs to be included in your room design: {selected_pieces}.",
+               },
+            ],
+         },
+      ],
+   )
+   #retrieve output from model 
+   description = response.choices[0].message.content
+   #generate image based on models description of the room 
+   generate_image(description)
+
+#function to generate description based on uploaded image input 
 #use a combination of chatgpt's vision capabilities and dall-e model to generate an image
 def generate_description_uploaded_input(image):
    #encode image 
@@ -86,7 +112,7 @@ def generate_description_uploaded_input(image):
       b64_image = base64.b64encode(image_file.read()).decode('utf-8')
    #make call to openai api with decoded image and prompt as input 
    response = client.chat.completions.create(
-      model="gpt-4o-mini",
+      model="gpt-4o",
       messages=[
          {
             "role": "user",
@@ -106,7 +132,7 @@ def generate_description_uploaded_input(image):
       ],
    )
    #extract text output from response 
-   img_description = (response.choices[0].message.content)
+   img_description = response.choices[0].message.content
 
    #after description is producted, call generate_image function to generate image using dalle-3 based on description 
    generate_image(img_description)
@@ -116,10 +142,10 @@ def generate_description_uploaded_input(image):
 @app.route('/main', methods =["GET", "POST"])
 def home():
    if request.method == "POST":
-      input_description = request.form.get("txtInput")
       input_img = request.form.get("imgInput")
+      aesthetic = request.form.get("aesthetic")
       #dictionary that get the status of the checkbox items (checked or unchecked)
-      #on = checked None = unchecked
+      #on = checked None = uncheckes
       furniture_items = {
          "coffee_table": request.form.get("coffeeTable"),
          "side_table": request.form.get("sideTable"),
@@ -134,16 +160,23 @@ def home():
          "desk": request.form.get("desk"),
          "armchair": request.form.get("armchair")
       }
+      #dictionary that will hold selected items as keys and filtered df as values 
+      selected_items = {}
       for piece in furniture_items:
-         print(furniture_items[piece])
+         #if value is on, then generate filtered df and add to selected dict 
+         if furniture_items[piece] == 'on':
+            filtered_df = piece_df(furniture_csv, keywords[piece], aesthetic)
+            selected_items[piece] = filtered_df
+         
       #if image is uploaded call uploaded_image function
       if input_img != "":
          #get uploaded image from HTML form 
          uploaded_image = request.form.get("imgInput")
          generate_description_uploaded_input(uploaded_image)
-      #if no uploaded image, make call to get_image which will generate image and save it to images dir
+      #if no uploaded image, generate description based on form inputs 
       else:
-         generate_image(input_description)
+         generate_description_user_input(selected_items.keys(), aesthetic)
+
    return render_template("index.html")
 
 if __name__=='__main__':
